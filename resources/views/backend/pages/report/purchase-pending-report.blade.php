70bb3eff0ccb21fc768aff8a3e86259d530964e9
mahfujurr221 <mahfujurr221@gmail.com>
2023-05-02 01:58:30 +0600

    modified

--- resources/views/backend/pages/report/purchase-pending-report.blade.php
+++ resources/views/backend/pages/report/purchase-pending-report.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.master')
 @section('content')
-    <h2 class="intro-y text-lg font-medium mt-10">Product Purchase Report</h2>
+    <h2 class="intro-y text-lg font-medium mt-10">Product Purchase Pending Report</h2>
     <div class="grid grid-cols-12 gap-6 mt-5">
         <div class="intro-y col-span-12 flex flex-wrap sm:flex-no-wrap items-center mt-2">
             <div class="hidden md:block mx-auto text-gray-600">
